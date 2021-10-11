cf15d1ea4eb4167d4abdb8f37bca07470d45acde
Michael Rider <mikepdxrider@gmail.com>
2021-10-11 13:20:15 -0700

    updates create-tables

--- data/create-tables.js
+++ data/create-tables.js
@@ -17,10 +17,10 @@ async function run() {
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
-                CREATE TABLE animals (
+                CREATE TABLE todos (
                     id SERIAL PRIMARY KEY NOT NULL,
                     name VARCHAR(512) NOT NULL,
-                    cool_factor INTEGER NOT NULL,
+                    completed BOOLEAN NOT NULL
                     owner_id INTEGER NOT NULL REFERENCES users(id)
             );
         `);
